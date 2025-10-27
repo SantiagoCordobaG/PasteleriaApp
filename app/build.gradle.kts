4fc3fff670b71f97887870b52ac0b615e9d04fc8
anthonyjsddkjsd <calderonguacamaya@gmail.com>
2025-10-27 11:54:38 -0500

    estructura limpia,casi sin warnings,carrito funcional,cuenta visual.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,7 @@
 plugins {
     id("com.android.application")
     alias(libs.plugins.kotlin.android)
-
-    //agregue este plugin de google services
     id("com.google.gms.google-services")
-
 }
 
 android {
@@ -15,8 +12,8 @@ android {
         applicationId = "com.anthonydevs.intento3.pasteleria"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -39,31 +36,26 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-    //dependencias android
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-
-    // Estas eran tus dependencias originales
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
-    //implementation("com.google.android.material:material:1.12.0") quite esta importacion
 
-    // Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-messaging")
+    implementation("com.google.firebase:firebase-firestore")
 
-    // Firebase productos
-    implementation("com.google.firebase:firebase-auth")       // Autenticación
-    implementation("com.google.firebase:firebase-messaging")  // Mensajería
-    implementation("com.google.firebase:firebase-firestore")  // Firestore
-
-
-    //test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/anthonydevs/intento3/pasteleria/producto.kt
+++
@@ -1,7 +0,0 @@
-package com.anthonydevs.intento3.pasteleria
-
-data class Producto(
-    val nombre: String,
-    val precio: String,
-    val imagenResId: Int
-)
